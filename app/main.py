867a05e71a88831a26e2e7c32c3c5d188745b04f
Valerii <valeraivasik602@gmail.com>
2026-02-02 22:32:05 +0000

    Implement Person class and create_person_list

--- app/main.py
+++ app/main.py
@@ -1,28 +1,25 @@
 class Person:
-    # Класовий словник для зберігання всіх людей за іменем
     people = {}
 
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
-        # Додаємо цей екземпляр у словник people
         Person.people[name] = self
 
 
 def create_person_list(people_dicts):
-    # Перший прохід: створюємо всі екземпляри Person
     person_list = []
-    for p in people_dicts:
-        person_list.append(Person(p["name"], p["age"]))
 
-    # Другий прохід: привязуємо атрибути wife/husband
-    for p_dict in people_dicts:
-        person = Person.people[p_dict["name"]]
-        # Привязуємо дружину, якщо є
-        if "wife" in p_dict and p_dict["wife"]:
-            person.wife = Person.people[p_dict["wife"]]
-        # Привязуємо чоловіка, якщо є
-        if "husband" in p_dict and p_dict["husband"]:
-            person.husband = Person.people[p_dict["husband"]]
+    for person_data in people_dicts:
+        person_list.append(Person(person_data["name"], person_data["age"]))
+
+    for person_data in people_dicts:
+        person = Person.people[person_data["name"]]
+
+        if "wife" in person_data and person_data["wife"]:
+            person.wife = Person.people[person_data["wife"]]
+
+        if "husband" in person_data and person_data["husband"]:
+            person.husband = Person.people[person_data["husband"]]
 
     return person_list
